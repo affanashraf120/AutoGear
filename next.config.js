ded8f53e334f4d8b53fd2e9558466ebbe988f425
affanashraf <affanashraf120@gmail.com>
2021-03-20 04:07:54 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,35 @@
-const locales = ['en', 'ru', 'ar'];
-const defaultLocale = 'en';
+const locales = ["en", "ru", "ar"];
+const defaultLocale = "en";
 
 // noinspection JSUnusedGlobalSymbols
-module.exports = {
-    env: {
-        basePath: process.env.BASE_PATH || '',
-    },
-    basePath: process.env.BASE_PATH || '',
-    async rewrites() {
-        return [
-            ...locales.filter((locale) => locale !== defaultLocale).map((locale) => [
-                { source: `/${locale}{/}?`, destination: '/' },
-                { source: `/${locale}/:path*`, destination: '/:path*' },
-            ]).reduce((acc, cur) => [...acc, ...cur], []),
-        ];
-    },
-    async redirects() {
-        return [
-            {
-                source: `/${defaultLocale}{/}?`,
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: `/${defaultLocale}/:path*`,
-                destination: '/:path*',
-                permanent: true,
-            },
-        ];
-    },
+export const env = {
+    basePath: process.env.BASE_PATH || "",
+    target: "experimental-serverless-trace",
 };
+export const basePath = process.env.BASE_PATH || "";
+export async function rewrites() {
+    return [
+        ...locales
+            .filter((locale) => locale !== defaultLocale)
+            .map((locale) => [
+                { source: `/${locale}{/}?`, destination: "/" },
+                { source: `/${locale}/:path*`, destination: "/:path*" },
+            ])
+            .reduce((acc, cur) => [...acc, ...cur], []),
+    ];
+}
+export async function redirects() {
+    return [
+        {
+            source: `/${defaultLocale}{/}?`,
+            destination: "/",
+            permanent: true,
+        },
+        {
+            source: `/${defaultLocale}/:path*`,
+            destination: "/:path*",
+            permanent: true,
+        },
+    ];
+}
+export const target = "experimental-serverless-trace";
