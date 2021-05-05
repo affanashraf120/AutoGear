a25f6bf94ce93b3dc4b4192317a76c3d505f7915
affanashraf <affanashraf120@gmail.com>
2021-05-05 07:02:56 +0500

    remove unused interfaces

--- src/components/shop/ShopPageProduct.tsx
+++ src/components/shop/ShopPageProduct.tsx
@@ -79,11 +79,11 @@ function ShopPageProduct(props: Props) {
 
     const breadcrumb = [
         { title: intl.formatMessage({ id: 'LINK_HOME' }), url: url.home() },
-        { title: intl.formatMessage({ id: 'LINK_SHOP' }), url: url.shop() },
-        ...getCategoryPath(product.categories && product.categories[0]).map((x) => ({
-            title: x.name,
-            url: url.category(x),
-        })),
+        // { title: intl.formatMessage({ id: 'LINK_SHOP' }), url: url.shop() },
+        // ...getCategoryPath(product.categories && product.categories[0]).map((x) => ({
+        //     title: x.name,
+        //     url: url.category(x),
+        // })),
         { title: product.name, url: url.product(product) },
     ];
 
