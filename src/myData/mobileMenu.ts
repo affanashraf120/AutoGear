5acd6b5d01ec72fc8a8138512da461a0718f856a
affanashraf <affanashraf120@gmail.com>
2022-04-02 23:04:10 +0500

    Update mobileMenu.ts

--- src/myData/mobileMenu.ts
+++ src/myData/mobileMenu.ts
@@ -1,32 +1,32 @@
-import { IMobileMenuLink } from "~/interfaces/mobile-menu-link";
+import { IMobileMenuLink } from '~/interfaces/mobile-menu-link';
 
 export const mobileMenu: IMobileMenuLink[] = [
     {
-        title: "Home",
-        url: "/",
+        title: 'Home',
+        url: '/',
     },
     {
-        title: "Find Cars",
-        url: "/find-cars",
+        title: 'Find Cars',
+        url: '/find-cars',
     },
     {
-        title: "Comparison",
-        url: "/compare",
+        title: 'Comparison',
+        url: '/compare',
     },
     {
-        title: "News",
-        url: "/news",
+        title: 'News',
+        url: '/news',
     },
     {
-        title: "Contact Us",
-        url: "/contact-us",
+        title: 'Contact Us',
+        url: '/contact-us',
     },
     {
-        title: "About Us",
-        url: "/about-us",
+        title: 'About Us',
+        url: '/about-us',
     },
     {
-        title: "FAQ",
-        url: "/faq",
+        title: 'FAQ',
+        url: '/faq',
     },
 ];
